e2d7718fd22129f4bee70046b0d57fb5fdcc21bc
Alitaimoor-10 <lapard666@gmail.com>
2026-01-04 10:41:10 +0500

    Tested timetable entry insertion in main program

---
+++ Smart timetable/src/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "timetable.h"
+
+using namespace std;
+
+int main()
+{
+    TimetableSlot timetable[10];
+
+    initializeTimetable(timetable, 10);
+
+    addTimetableEntry(
+        timetable,
+        10,
+        "Programming Fundamentals",
+        "Sir Nouman",
+        "Lab 1",
+        0,
+        1
+    );
+
+    viewTimetable(timetable, 10);
+
+    return 0;
+}
