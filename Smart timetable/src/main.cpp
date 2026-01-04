6206b1f91a5a93e3546f562cb8c58b2e228e194c
abdullah00192 <009abdullah@gmail.com>
2026-01-04 19:40:35 +0500

    Added menu-driven main function with save option

--- Smart timetable/src/main.cpp
+++ Smart timetable/src/main.cpp
@@ -5,28 +5,46 @@ using namespace std;
 
 
 
-
-
-
-
-
 int main()
 {
     TimetableSlot timetable[10];
+    int choice;
 
     initializeTimetable(timetable, 10);
 
-    addTimetableEntry(
-        timetable,
-        10,
-        "Programming Fundamentals",
-        "Sir Nouman",
-        "Lab 1",
-        0,
-        1
-    );
+    do
+    {
+        cout << "\n===== TIMETABLE MANAGEMENT SYSTEM =====\n";
+        cout << "1. Add Timetable Entry\n";
+        cout << "2. View Timetable\n";
+        cout << "3. Save Timetable to File\n";
+        cout << "0. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+        cin.ignore();
 
-    viewTimetable(timetable, 10);
+        if (choice == 1)
+        {
+            addTimetable(timetable, 10);
+        }
+        else if (choice == 2)
+        {
+            viewTimetable(timetable, 10);
+        }
+        else if (choice == 3)
+        {
+            saveTimetableToFile(timetable, 10);
+        }
+        else if (choice == 0)
+        {
+            cout << "Exiting program...\n";
+        }
+        else
+        {
+            cout << "Invalid choice!\n";
+        }
+
+    } while (choice != 0);
 
     return 0;
 }
